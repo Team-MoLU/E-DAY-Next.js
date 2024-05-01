edbbbf5d6e7616678e903c22f6354599f67a54cf
easyoungcode <gchun910@gmail.com>
2024-05-01 19:48:12 +0900

    init: 프로젝트 생성

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Counter from '../components/Counter';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js with Redux</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default HomePage;

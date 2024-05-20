ae1064f9e3b02da9672b83e50cb7ff74628d7f90
yachae1101 <yachae1101@naver.com>
2024-05-21 04:41:40 +0900

    [develop] chore: ip 활용 api fetch

--- src/app/api-test/page.js
+++ src/app/api-test/page.js
@@ -1,5 +1,6 @@
 export default async function ApiTestPage() {
-  const apiText = await fetch("http://spring-container:8080/test")
+  // const apiText = await fetch("http://spring-container:8080/test")
+  const apiText = await fetch("http://3.38.4.187:8080/test")
     .then((response) => response.text())
     .catch(() => "API Test fail");
 

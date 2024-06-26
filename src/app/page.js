2c7c682476448e56b9501b704cf0d94a1d2bee70
yachae1101 <yachae1101@naver.com>
2024-06-26 16:10:59 +0900

    [login] feat: cookie 에 token 정보 없으면 login으로 리다이렉트

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,33 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { redirect } from "next/navigation";
 
 export default function RootPage() {
-  // 클라이언트 사이드에서 리다이렉션을 처리합니다.
-  redirect("/app");
+  const [accessToken, setAccessToken] = useState(null);
+  const [refreshToken, setRefreshToken] = useState(null);
+
+  useEffect(() => {
+    function getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(";").shift();
+      return null;
+    }
+
+    const accessCookie = getCookie("access");
+    const refreshCookie = getCookie("refresh");
+
+    setAccessToken(accessCookie);
+    setRefreshToken(refreshCookie);
+  }, []);
+
+  if (accessToken === null || refreshToken === null) {
+    // cookie 에 accessToken 또는 refreshToken 정보가 없으면 login으로 리다이렉트
+    redirect("/login");
+  } else {
+    redirect("/app");
+  }
   // 이 페이지는 실제로 렌더링되지 않습니다.
   return null;
 }

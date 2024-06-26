d658b91c620018f2d11d098fd8a4bbed769e4fbb
yachae1101 <yachae1101@naver.com>
2024-06-26 15:54:15 +0900

    [login] feat: root에서 app으로 리다이렉트 설정 및 로그인 추가

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,44 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+
 export default function LoginPage() {
+  const [accessToken, setAccessToken] = useState(null);
+  const [refreshToken, setRefreshToken] = useState(null);
+
+  useEffect(() => {
+    console.log("useEffect is running");
+
+    function getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(";").shift();
+      return null;
+    }
+
+    const accessCookie = getCookie("access");
+    const refreshCookie = getCookie("refresh");
+
+    setAccessToken(accessCookie);
+    setRefreshToken(refreshCookie);
+  }, []);
+
+  useEffect(() => {
+    console.log("Access Token:", accessToken);
+    console.log("Refresh Token:", refreshToken);
+  }, [accessToken, refreshToken]);
+
   return (
     <div>
-      <h1>This is Login page</h1>
+      <Link href="http://localhost:8080/login/oauth2/code/google">
+        <button>회원가입</button>
+      </Link>
+      <br></br>
+      <br></br>
+      <Link href="http://localhost:8080/login">
+        <button>로그인</button>
+      </Link>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
+import { redirect } from "next/navigation";
+
 export default function RootPage() {
-  return (
-    <div>
-      <h1>This is Root page</h1>
-    </div>
-  );
+  // 클라이언트 사이드에서 리다이렉션을 처리합니다.
+  redirect("/app");
+  // 이 페이지는 실제로 렌더링되지 않습니다.
+  return null;
 }
